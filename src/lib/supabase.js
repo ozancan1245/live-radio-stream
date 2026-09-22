31e53afa60e7c49e9119b8d0751447d8dd827191
ozancan1245 <keygenonline27@gmail.com>
2026-09-22 23:02:28 +0300

    Supabase client fallback eklendi

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  'https://kgqotwnboauopvxwouik.supabase.co';
+
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtncW90d25ib2F1b3B2eHdvdWlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY4ODg4ODAsImV4cCI6MjAzMjQ2NDg4MH0.placeholder';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
